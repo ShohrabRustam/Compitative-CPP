78e28963631619781aed4b5c835204bb652b8cc5
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-06 10:10:31 +0530

    Valid Triangle

---
+++ 6ValidTriangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int side1,side2,side3;
+    cout<<"Enter side of the triangle"<<endl;
+    cin>>side1>>side2>>side3;
+    if(side1+side3>side2  && side1+side2>side3 && side2+side3>side1)
+    cout<<"Triangle is Valid triangle"<<endl;
+    cout << "Triangle is not valid"<<endl;
+    return 0;
+}
