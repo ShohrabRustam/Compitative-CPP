f530c0365f3fdc2f0a4f91411105992e8aa6aa12
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-06 12:42:15 +0530

    conditioanl

---
+++ Basic/conditional.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    if(n>0){
+        cout<<n<<" is Positive"<<endl;
+    }else if(n==0){
+        cout<<n<<" is Zero"<<endl;
+    }
+    else{
+        cout<<n<<" is Negative"<<endl;
+    }
+return 0;
+}
