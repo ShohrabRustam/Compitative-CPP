17bdab8b5344ad8986d6085493c2e67169395372
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-06 10:05:01 +0530

    basic program

---
+++ 4greaterNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+    int n; 
+    int m ;
+    cout << "Enter two numbers: " << endl;
+    if(n>m){
+        cout << "Greater numbers: " <<n<< endl;
+    }else{
+        cout << "Greater numbers: "<<m << endl;
+    }
+    return 0;
+}
