0caba4f8909257a369fddc377b01be204e6549fb
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-06 10:12:14 +0530

    Valid Triangle

--- 6ValidTriangle.cpp
+++ 6ValidTriangle.cpp
@@ -5,8 +5,10 @@ int main()
     int side1,side2,side3;
     cout<<"Enter side of the triangle"<<endl;
     cin>>side1>>side2>>side3;
-    if(side1+side3>side2  && side1+side2>side3 && side2+side3>side1)
+    if(side1+side3>side2  && side1+side2>side3 && side2+side3>side1){
     cout<<"Triangle is Valid triangle"<<endl;
+    }else{
     cout << "Triangle is not valid"<<endl;
+    }
     return 0;
 }
