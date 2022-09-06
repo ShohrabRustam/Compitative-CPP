b4cf756f61167f8b862b32c3f2158f3574af7164
Shohrab Rustam <mohdrustam001@gmail.com>
2022-09-06 10:51:45 +0530

    datatype

---
+++ Basic/dataType.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char** argv){
+    int a=123;
+    cout<<"Size of "<<a<<" "<<sizeof(a)  <<"\n";
+    char c='a';
+    cout<<c<<"\n";
+    bool b=true;
+    cout<<b<<"\n";
+    float f=1.230f;
+    cout<<f<<"\n";
+    double d=1.23;
+    cout << d<<"\n";
+    return 0;
+}
